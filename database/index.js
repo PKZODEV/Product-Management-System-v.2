aec3d1d9821a0151e344cd9a37f49fc08c1f1d60
PKZODEV <qapwork24hr@gmail.com>
2023-10-16 14:24:34 +0700

    update

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+});
 
-  module.exports = pool.promise()
+module.exports = pool.promise();
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const productsRouter = require('./routes/products.router')
+const productsRouter = require("./routes/products.router");
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use('/products', productsRouter)
+app.use("/products", productsRouter);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, () =>{
-    console.log(`Server is Running.....`)
+app.listen(PORT, () => {
+  console.log(`Server is Running.....`);
 });
